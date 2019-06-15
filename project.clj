b6017bc5ba18c1d12716393ab37c3383a745a67a
K B <kbosompem@gmail.com>
2019-06-15 06:28:11 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-aws-s3 "0.3.10"
+(defproject clj-aws-s3 "0.4.20190615"
   :description "Clojure Amazon S3 library"
   :url "https://github.com/weavejester/clj-aws-s3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.7.5"]
-                 [clj-time "0.6.0"]]
-  :plugins [[codox "0.8.10"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.amazonaws/aws-java-sdk "1.11.572"]
+                 [clj-time "0.15.0"]]
+  :plugins [[codox "0.10.7"]])
