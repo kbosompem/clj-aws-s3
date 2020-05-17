b955010f975ad3e129f8d991ad0caf192e15e1c7
Kay Bosompem <kbosompem@enstoa.com>
2020-05-17 10:03:12 -0500

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject org.clojars.kbosompem/clj-aws-s3 "0.4.191203"
+(defproject org.clojars.kbosompem/clj-aws-s3 "0.5.20200517"
   :description "Clojure Amazon S3 library by Weavejester"
   :url "https://github.com/kbosompem/clj-aws-s3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [commons-logging/commons-logging "1.2"]
-                 [com.fasterxml.jackson.core/jackson-core "2.10.1"]
-                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.10.1"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.688" :exclusions [com.fasterxml.jackson.core/jackson-core
+                ; [commons-logging/commons-logging "1.2"]
+                ; [com.fasterxml.jackson.core/jackson-core "2.11.0"]
+                ; [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.11.0"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.784" :exclusions [com.fasterxml.jackson.core/jackson-core
                                                                         com.fasterxml.jackson.dataformat/jackson-dataformat-cbor
                                                                         commons-logging]]
                  [clj-time "0.15.2"]]
